4e0cc9123064bdbc380c92f7e61bed3cbf4a8e67
filipdespic <despicpb12@gmail.com>
2023-12-14 14:13:17 +0100

    add sending data via path variables

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -3,13 +3,14 @@ package com.example.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/home")
-    public String home(@RequestParam String username, @RequestParam String color, Model page) {
+    @GetMapping("/home/{username}/{color}")
+    public String home(@PathVariable String color, @PathVariable String username, Model page) {
         page.addAttribute("username", username);
         page.addAttribute("color", color);
         return "home.html";
