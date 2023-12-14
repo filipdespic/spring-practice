8919f7c21e04bfe6b9b9632c017f012882eec108
filipdespic <despicpb12@gmail.com>
2023-12-14 14:04:52 +0100

    add sending data via request parameters

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -3,14 +3,15 @@ package com.example.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/home")
-    public String home(Model page) {
-        page.addAttribute("username", "Katy");
-        page.addAttribute("color", "red");
+    public String home(@RequestParam String username, @RequestParam String color, Model page) {
+        page.addAttribute("username", username);
+        page.addAttribute("color", color);
         return "home.html";
     }
 
