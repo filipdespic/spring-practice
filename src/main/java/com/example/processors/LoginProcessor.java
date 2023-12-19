4c7b8b4715103c8dfc44cdfcaa90cd3facec2d7e
filipdespic <despicpb12@gmail.com>
2023-12-19 13:28:42 +0100

    add primitive login to demonstrate request bean scope

---
+++ src/main/java/com/example/processors/LoginProcessor.java
@@ -0,0 +1,41 @@
+package com.example.processors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class LoginProcessor {
+
+    private String username;
+    private String password;
+
+    public boolean login() {
+        String username = this.getUsername();
+        String password = this.getPassword();
+
+        System.out.println(username + password);
+
+        if ("a".equals(username) && "b".equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
