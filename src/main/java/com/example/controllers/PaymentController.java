a110231ae401ffa59817cb91ef6b017f457c36a3
filipdespic <despicpb12@gmail.com>
2024-01-04 18:17:57 +0100

    add payment endpoint and exception controller advice

--- src/main/java/com/example/controllers/PaymentController.java
+++ src/main/java/com/example/controllers/PaymentController.java
@@ -7,26 +7,25 @@ import com.example.services.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
 
     private final PaymentService paymentService;
 
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
+
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment(){
-        try {
-            PaymentDetails paymentDetails = paymentService.processPayment();
-            return ResponseEntity.status(HttpStatus.ACCEPTED).body(paymentDetails);
-        } catch (NotEnoughMoneyException e) {
-            ErrorDetails errorDetails = new ErrorDetails();
-            errorDetails.setMessage("Not enough money to make the payment.");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetails);
-        }
+    public ResponseEntity<PaymentDetails> makePayment(@RequestBody PaymentDetails paymentDetails){
+        logger.info("Received payment " + paymentDetails.getAmount());
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(paymentDetails);
     }
 }
---
+++ src/main/java/com/example/exceptions/ExceptionControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.example.exceptions;
+
+import com.example.model.ErrorDetails;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(NotEnoughMoneyException.class)
+    public ResponseEntity<ErrorDetails> exceptionNotEnoughMoneyHandler() {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage("Not enough money to make the payment.");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetails);
+    }
+}
