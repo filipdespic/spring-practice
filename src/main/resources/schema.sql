2e522ec6bcbb9e9df9e0e309f423d394147ed028
filipdespic <despicpb12@gmail.com>
2024-01-27 14:44:52 +0100

    add integration test for transfer service

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS purchase (
  product varchar(50) NOT NULL,
  price double NOT NULL
 );
-create table account (
+CREATE TABLE IF NOT EXISTS account (
  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
  name VARCHAR(50) NOT NULL,
  amount DOUBLE NOT NULL
